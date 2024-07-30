ccfd43789db68e1bb8f7c354511ed955249e9797
debangan13 <debangan.barmanslg@gmail.com>
2024-07-30 12:33:07 +0530

    change the db connection

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import mongoose from "mongoose"
+import connectDB from "./db/connect.js"
+
 dotenv.config()
 
 const app = express() 
 const port = process.env.PORT || 3001
 
-const server = app.listen(port, ()=> {
-    console.log(`Server is running at PORT ${port}...`)
-})
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    credentials:true,
+}));
+
+app.use(cookieParser());
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI )
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
