20207be29a2f19640084705d118f9d2217e8516e
debagnan <debangan.barmanslg@gmail.com>
2024-08-03 21:20:26 +0530

    added login

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -1,22 +1,52 @@
-import User from "../models/UserModel.js"
-import { attachCookiesToResponse, createJWT, createTokenUser } from "../utils/index.js";
+import User from "../models/UserModel.js";
+import {
+  attachCookiesToResponse,
+  createJWT,
+  createTokenUser,
+} from "../utils/index.js";
 
-export const signup = async (req,res,next) => {
-    try {
-        const {email,password} = req.body
-        if(!email && !password) return
-        const user = await User.create({...req.body})
-        const tokenUser = createTokenUser(user)
-        attachCookiesToResponse({res,user:tokenUser})
-
-        return res.status(201).json({user:{
-            id:user._id,
-            email:user.email,
-            profileSetup:user.ProfileSetup
-        }});
-        
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send("Interna server error")
+// for user signup
+export const signup = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email && !password) {
+      return res.status(400).send("Email and Password required");
     }
-}
+    const user = await User.create({ ...req.body });
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser });
+
+    return res.status(201).json({
+      user: {
+        id: user._id,
+        email: user.email,
+        profileSetup: user.ProfileSetup,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Interna server error");
+  }
+};
+
+// for user login
+export const login = async (req, res) => {
+  try {
+    console.log('in login')
+    const { email, password } = req.body;
+    console.log(email,password);
+    if (!email && !password) {
+
+      return res.status(400).send("Email and Password required");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("Email not found");
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+    console.log(isPasswordCorrect) 
+    console.log("hello ");
+
+  } catch (error) {}
+};
