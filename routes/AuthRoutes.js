a85bf1586a1bc2e032af7a88a5a0f3e1cd7f3fdf
debangan13 <debangan.barmanslg@gmail.com>
2024-08-13 23:18:21 +0530

    added getUserInfo controller and middleware

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { getUserInfo, login, signup } from '../controllers/AuthControllers.js'
+import { getUserInfo, login, signup,updateProfile } from '../controllers/AuthControllers.js'
 import { authenticatedUser } from '../middlewares/authentication.js'
 
 const router = Router()
 
 router.post('/signup',signup)
 router.post('/login',login)
-router.get('/user-info',getUserInfo)
+router.get('/user-info',authenticatedUser,getUserInfo)
+router.post('/update-profile',authenticatedUser,updateProfile)
 
 export default router
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,21 +1,23 @@
 import jwt from "jsonwebtoken";
 
-export const createJWT = ({ payload }) => {0.
-  
-  const token = jwt.sign(payload, process.env.JWT_SCERET, {
-    expiresIn:"30d",
-  });
-  return token;
+export const createJWT = ({ payload }) => {
+	const token = jwt.sign(payload, process.env.JWT_SECRET, {
+		expiresIn: "30d",
+	});
+	return token;
 };
 
-export const attachCookiesToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+export const isTokenValid = ({ token }) =>
+	jwt.verify(token, process.env.JWT_SECRET);
 
-  const oneDay = 1000 * 60 * 60 * 24;
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-            secure: true,
-            sameSite:"None"
-  });
+export const attachCookiesToResponse = ({ res, user }) => {
+	const token = createJWT({ payload: user });
+
+	const oneDay = 1000 * 60 * 60 * 24;
+	res.cookie("token", token, {
+		httpOnly: true,
+		expires: new Date(Date.now() + oneDay),
+		secure: true,
+		sameSite: "None",
+	});
 };
