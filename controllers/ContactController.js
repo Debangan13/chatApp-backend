7b98a295800a98573d9d74f0fea2a7c92a12339b
debangan13 <debangan.barmanslg@gmail.com>
2024-08-23 22:51:36 +0530

    change the searchContacts

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -2,8 +2,11 @@ import User from "../models/UserModel.js";
 
 export const searchContacts = async (req, res) => {
 	try {
-		const { searchTerm } = req.body;
-        console.log(searchTerm);
+		const {
+			body: { searchTerm },
+			user: { userId },
+		} = req;
+
 		if (searchTerm === undefined || searchTerm === null) {
 			return res.status(400).send("searchTerm is requried");
 		}
@@ -16,13 +19,31 @@ export const searchContacts = async (req, res) => {
 		const regex = new RegExp(sanitizedSearchTerm, "i");
 		const contacts = await User.find({
 			$and: [
-				{ _id: { $ne: req.userId } },
+				{ _id: { $ne: userId } },
 				{
 					$or: [{ firstName: regex }, { lastName: regex }, { email: regex }],
 				},
 			],
 		});
-        return res.status(200).json({contacts})
+		
+		// const searchParts = searchTerm.trim().split(/\s+/);
+
+		// const regexArray = searchParts.map(
+		// 	(part) => new RegExp(part.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), "i")
+		// );
+
+		// const contacts = await User.find({
+		// 	$and: [
+		// 		{ _id: { $ne: userId } },
+		// 		{
+		// 			$or: regexArray.map((regex) => ({
+		// 				$or: [{ firstName: regex }, { lastName: regex }, { email: regex }],
+		// 			})),
+		// 		},
+		// 	],
+		// });
+
+		return res.status(200).json({ contacts });
 	} catch (error) {
 		console.log(error.message);
 	}
